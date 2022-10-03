961054abad6367d0e5f14f378a60dbe05030f272
khrystynadol <khrystyna.dolynska.kn.2021@lpnu.ua>
2022-10-03 14:50:21 +0300

    Adding database and models.py sorted in folder website

--- main.py
+++ main.py
@@ -1,31 +1,4 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from os import path
-
-db = SQLAlchemy()
-DB_NAME = "database.db"
-
-
-def create_app():
-    myapp = Flask(__name__)
-    myapp.config['SECRET_KEY'] = 'Lorem ipsum dolor sit amet.'
-    myapp.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(myapp)
-
-    from LeisureGuruBE.home import home_page
-    myapp.register_blueprint(home_page, url_prefix='/')
-
-    create_database(myapp)
-
-    return myapp
-
-
-def create_database(myapp):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=myapp)
-        print('Created Database!')
-
-
+from website import create_app
 app = create_app()
 
 
---
+++ website/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+def create_app():
+    myapp = Flask(__name__)
+    myapp.config['SECRET_KEY'] = 'Lorem ipsum dolor sit amet.'
+    myapp.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(myapp)
+
+    from home import home_page
+    myapp.register_blueprint(home_page, url_prefix='/')
+
+    create_database(myapp)
+
+    return myapp
+
+
+def create_database(myapp):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=myapp)
+        print('Created Database!')
+
